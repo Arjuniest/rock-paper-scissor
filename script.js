f1c84fac3229c8b2b9a58b91b5755472dc29a880
Arjuniest <thedivinetreee@gmail.com>
2022-04-27 14:05:08 +0530

    Add function to play single round of Rock Paper Scissors

--- script.js
+++ script.js
@@ -1,13 +1,65 @@
 function counterPlay(){
     let computerChoice = Math.random(); //returns an integer 0 to 1
     if(computerChoice <= 0.34){
-        return "Rock";
+        return "rock";
     }
     else if(computerChoice <= 0.67){
-        return "Paper";
+        return "paper";
     }
-    else return "Scissors"
+    else return "scissors"
 }
 
 // console.log(counterPlay());
 
+function yourTurn(){
+    let turn = prompt("Choose among rock paper or scissors: ");
+    return turn;
+}
+
+function playRound(playerSelection, computerSelection){
+    computerSelection = counterPlay();
+    playerSelection = yourTurn();
+    console.log(playerSelection);
+    console.log(computerSelection);
+    if (playerSelection === 'scissors'){
+        if (computerSelection === 'scissors'){
+            console.log( 'It is a tie');
+        }
+        else if (computerSelection === 'rock'){
+            console.log('You loose. Rock beat scissors');
+        }
+        else if (computerSelection === 'paper'){
+            console.log('You win! scissors beat paper');
+        }
+    }
+
+    else if (playerSelection === 'rock'){
+        if (computerSelection === 'rock'){
+            console.log('It is a tie');
+        }
+        else if (computerSelection === 'paper'){
+            console.log('You loose. Paper beat rock.');
+        }
+        else if (computerSelection === 'scissors'){
+            console.log('You win! Rock beat scissors');
+        }
+    }
+
+    else if (playerSelection === 'paper'){
+        if (computerSelection === 'paper'){
+            console.log('It is a tie.');
+        }
+        if (computerSelection === 'scissors'){
+            console.log('You loose. Scissors beat paper.');
+        }
+        if (computerSelection === 'rock'){
+            console.log('You win! Paper beat rock.');
+        }
+    }
+
+    else console.log('The world is beautiful');
+}
+
+
+counterPlay();
+playRound();
